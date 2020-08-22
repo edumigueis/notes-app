058e27123fd532300a93a828bd5f93adc21e47e4
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-22 15:38:49 -0300

    Basic editing notes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 let notes = [
   {
     title: "Carol's Party",
-    text: "Bring her a gift and buy soda. yhyi891gt5a0 I'm thinking of buying a really big gift because i wanna show all my love for her. Maybe a biiggg teddy bear would be perfect: yhyi891gt5a1",
+    text:
+      "Bring her a gift and buy soda. yhyi891gt5a0 I'm thinking of buying a really big gift because i wanna show all my love for her. Maybe a biiggg teddy bear would be perfect: yhyi891gt5a1",
     images: ["./images/1.jpg", "./images/2.jpg"],
   },
   {
@@ -23,27 +24,54 @@ function openNote(ev) {
   currentNote = notes[ev.target.dataset.note];
   document.getElementById("current-title").innerHTML = currentNote.title;
   document.getElementById("current-note").innerHTML = currentNote.text;
-  if(currentNote.images.length > 0){
-        let res = "";
-        let currentString;
-        let minusCurrentString;
-        for(var i = 0; i < currentNote.images.length; i++){
-            currentString = "yhyi891gt5a"+i;
-            minusCurrentString = "yhyi891gt5a"+ (i-1)
-            alert(minusCurrentString);
-            if(i == 0){
-                res += currentNote.text.slice(0, currentNote.text.indexOf(currentString)) + "<img src="+currentNote.images[i] +" />";
-                
-            } else{
-                res += currentNote.text.substring(currentNote.text.indexOf(minusCurrentString) + minusCurrentString.length) + "<img src="+currentNote.images[i] +" />";
-                console.log(res);
-            }
-        }
-        for(var a = 0; a < currentNote.images.length; a++){
-            res = res.replace("yhyi891gt5a"+a,'');
-        }
-        
+  if (currentNote.images.length > 0) {
+    let res = "";
+    let currentString;
+    let minusCurrentString;
+    for (var i = 0; i < currentNote.images.length; i++) {
+      currentString = "yhyi891gt5a" + i;
+      minusCurrentString = "yhyi891gt5a" + (i - 1);
+      alert(minusCurrentString);
+      if (i == 0) {
+        res +=
+          currentNote.text.slice(0, currentNote.text.indexOf(currentString)) +
+          "<div class='img-wp'><img src=" +
+          currentNote.images[i] +
+          " /><div>";
+      } else {
+        res +=
+          currentNote.text.substring(
+            currentNote.text.indexOf(minusCurrentString) +
+              minusCurrentString.length
+          ) +
+          "<div class='img-wp'><img src=" +
+          currentNote.images[i] +
+          " /><div>";
+        console.log(res);
+      }
+    }
+    for (var a = 0; a < currentNote.images.length; a++) {
+      res = res.replace("yhyi891gt5a" + a, "");
+    }
 
-        document.getElementById("current-note").innerHTML = res;
+    document.getElementById("current-note").innerHTML = res;
+  }
+}
+
+document
+  .getElementById("current-note")
+  .addEventListener("dblclick", function (event) {
+    editNote(event);
+  });
+document.getElementById("edit-note-btn").addEventListener("click", function () {
+  editNote(document.getElementById("current-note"));
+});
+
+function editNote(ev) {
+  if (ev.target == undefined) {
+    ev.contentEditable = true;
+    ev.focus();
+  } else {
+    ev.target.contentEditable = true;
   }
 }
